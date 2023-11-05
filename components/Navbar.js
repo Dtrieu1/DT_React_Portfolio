f19ed5c4fea2b892c52e3520dc9789a3ec9894bf
David <131110984+Dtrieu1@users.noreply.github.com>
2023-11-05 13:48:25 -0600

    updated component, src, and public structure

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TESTING
+                    </Link>
+                </div>
+            </nav> 
+        </div>
+    )
+}
+
+export default Navbar
